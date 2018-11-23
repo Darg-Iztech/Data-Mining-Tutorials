8fd91d772fbc9f394b909e7f7719b937d8f0ca15
polatbilek <polatbilekozan@hotmail.com>
2018-11-23 17:06:21 +0300

    example 5 is fully complete now

--- week1/5_contingency_table.py
+++ week1/5_contingency_table.py
@@ -2,19 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-colnames=['Sepal.Length','Sepal_Width','Petal.Length','Petal.Width','Species']
+colnames=['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Species']
 d = pd.read_csv('iris.txt',names=colnames)
 
-
-discriminator_feature = 'Sepal.Width'
-threshold = 3
+# initialization of parameters
+discriminator_feature = 'Sepal_Width'
+target_class = "versicolor"
+threshold = 2.8
 
 prediction = []
 values = d.get_values()
 
+# prediction
 for i in range(len(values)):
-	if int(values[i][1]) < threshold:
-		prediction.append(["versicolor",values[i][4]])
+	if float(values[i][colnames.index(discriminator_feature)]) < threshold: # colnames.index(discriminator_feature) gives index of feature
+		prediction.append([target_class,values[i][4]])
 	else:
 		prediction.append(["other",values[i][4]])
 
@@ -26,16 +28,19 @@ fp = 0
 tn = 0
 fn = 0
 
+# Calculating value of contingency table cells
 for i in range(len(prediction)):
-	if prediction[i][0] == 'other' and (prediction[i][1] != 'versicolor'):
+	if prediction[i][0] == 'other' and (prediction[i][1] != target_class):
 		tn += 1
-	elif prediction[i][0] == 'other' and (prediction[i][1] == 'versicolor'):
+	elif prediction[i][0] == 'other' and (prediction[i][1] == target_class):
 		fn += 1
-	elif prediction[i][0] == 'versicolor' and (prediction[i][1] == 'versicolor'):
+	elif prediction[i][0] == target_class and (prediction[i][1] == target_class):
 		tp += 1
-	elif prediction[i][0] == 'versicolor' and (prediction[i][1] != 'versicolor'):
+	elif prediction[i][0] == target_class and (prediction[i][1] != target_class):
 		fp += 1
 
+
+# Cool printing
 print(str("\n   ")+str(1)+  "  |"  +str("  ")+str(  0))
 print(str(1)+ "| " +str(tp) +str("   ")+  str(fn))
 print(str(0)+ "| " +str(fp) + str("   ")+  str(tn))
@@ -45,8 +50,8 @@ jaccard_coefficient = tp/(fp+tp+fn)
 print("\nJaccard Coefficient: ",jaccard_coefficient)
 
 
-
-table = pd.crosstab(d['Sepal_Width']<3, d['Species']=='versicolor', margins=True)
+# An easier way to do the things above
+table = pd.crosstab(d[discriminator_feature]<threshold, d['Species']==target_class, margins=True)
 print("\n",table)
 
 jaccard_coefficient = table.values[1][1]/(table.values[0][1]+table.values[1][1]+table.values[1][0])
